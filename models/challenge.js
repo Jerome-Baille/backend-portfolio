c69582f5a476f8568d0bb17f11141aeb0a78678f
Jerome-Baille <baille.jerome@gmail.com>
2022-08-28 19:55:36 +0200

    Modify challenges name and url

--- models/challenge.js
+++ models/challenge.js
@@ -1,25 +1,23 @@
 const challenges = [
     {
-        id: 1,
+        id: "HundredDaysOfCSS",
         status: 'Active',
         name: '100 days CSS Challenge',
         description: [
             "The challenge is to reproduce a given HTML and CSS snippets for 100 consecutive days.",
             "Participants use CodePen to write and save their code and publish the results every day on the challenge website."
         ],
-        website: 'https://100dayscss.com/',
-        url: '#'
+        url: 'https://100dayscss.com/'
     },
     {
-        id: 2,
+        id: "GoogleKickStart2022",
         status: 'Active',
         name: 'Google Kick Start 2022',
         description: [
             "Kick Start offers coders around the world the chance to develop and hone their programming skills through online-hosted competition rounds.",
             "The three-hour rounds feature a variety of algorithmic challenges, all developed by Google engineers so that you get a taste of the technical skills needed for a career at Google (the top competitors from our Kick Start rounds may be invited to interview at Google!)."
         ],
-        website: 'https://codingcompetitions.withgoogle.com/kickstart/about',
-        url: '#'
+        url: 'https://codingcompetitions.withgoogle.com/kickstart/about'
     }
 ];
 
--- models/project.js
+++ models/project.js
@@ -1,78 +1,78 @@
 const projects = {
         "PhoceaTravel":{
-            "id": "PhoceaTravel",
-            "logo": "/Projects/PhoceaTravel/phocea-travel-logo.png",
-            "pictures": [
+            id: "PhoceaTravel",
+            logo: "/Projects/PhoceaTravel/phocea-travel-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/PhoceaTravel/phocea-travel-desktop.png",
+                    url: "/Projects/PhoceaTravel/phocea-travel-desktop.png",
                     "alt": "Desktop Template"
                 },
                 {
-                    "url": "/Projects/PhoceaTravel/phocea-travel-mobile.png",
-                    "caption": "Mobile Template"
+                    url: "/Projects/PhoceaTravel/phocea-travel-mobile.png",
+                    caption: "Mobile Template"
                 }
             ],
-            "name": "Phocéa Travel",
-            "subtitle": "Responsive website for a travel agency",
-            "descriptions" : [
+            name: "Phocéa Travel",
+            subtitle: "Responsive website for a travel agency",
+            descriptions : [
                 "Phocéa Travel is a small company offering a vacation planning tool!",
                 "Its website allows users to find accommodations and activities in the city of their choice.",
                 "Accommodations can also be filtered by theme, such as budget or ambiance."
             ],
-            "skills": [
+            skills: [
                 "Integrate content according to a template",
                 "Implement a responsive interface"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "React",
-                    "url": "/Stack/react.svg"
+                    name: "React",
+                    url: "/Stack/react.svg"
                 },
                 {
-                    "name": "HTML 5",
-                    "url": "/Stack/html5.svg"
+                    name: "HTML 5",
+                    url: "/Stack/html5.svg"
                 },
                 {
-                    "name": "CSS 3",
-                    "url": "/Stack/css3.svg"
+                    name: "CSS 3",
+                    url: "/Stack/css3.svg"
                 },
                 {
-                    "name": "Sass",
-                    "url": "/Stack/sass.svg"
+                    name: "Sass",
+                    url: "/Stack/sass.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Make the search bar functional",
                 "Add a database to turn the static website into a modern dynamic app",
                 "Link the website to google maps API to show the location of the accommodations",
                 "Add a login system to allow users to book accommodations"
             ],
-            "tags": "#React #HTML5  #CSS3 #Sass",
-            "github": "https://github.com/Jerome-Baille/phocea-travel",
-            "demo": "https://jerome-baille.github.io/phocea-travel/"
+            tags: "#React #HTML5  #CSS3 #Sass",
+            github: "https://github.com/Jerome-Baille/phocea-travel",
+            demo: "https://jerome-baille.github.io/phocea-travel/"
         },
         "OhMyFood": {
-            "id": "OhMyFood",
-            "logo": "/Projects/OhMyFood/ohMyFood-logo.png",
-            "pictures": [
+            id: "OhMyFood",
+            logo: "/Projects/OhMyFood/ohMyFood-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/OhMyFood/mokup1.png",
+                    url: "/Projects/OhMyFood/mokup1.png",
                     "alt": "OhMyFood - Mokup 1"
                 },
                 {
-                    "url": "/Projects/OhMyFood/mokup2.png",
-                    "caption": "OhMyFood - Mokup 2"
+                    url: "/Projects/OhMyFood/mokup2.png",
+                    caption: "OhMyFood - Mokup 2"
                 }
             ],
-            "name": "OhMyFood",
-            "subtitle": "Dynamizing a web page with CSS animations",
-            "descriptions" : [
+            name: "OhMyFood",
+            subtitle: "Dynamizing a web page with CSS animations",
+            descriptions : [
                 "Ohmyfood! is a young startup that would like to impose itself on the restaurant market.", 
                 "The objective is to develop a 100% mobile website that lists the menus of gourmet restaurants.", 
                 "In addition to the classic reservation systems, customers will be able to compose the menu of their meal so that the dishes are ready when they arrive.", 
                 "No more waiting in restaurants!"
             ],
-            "skills": [
+            skills: [
                 "Implementing advanced graphic CSS effects",
                 "Using a version management system for project tracking and hosting",
                 "Ensuring the graphic coherence of a website",
@@ -80,223 +80,223 @@ const projects = {
                 "Setting up a navigation structure for a website", 
                 "Using a version management system for project monitoring and hosting"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "React",
-                    "url": "/Stack/react.svg"
+                    name: "React",
+                    url: "/Stack/react.svg"
                 },
                 {
-                    "name": "HTML 5",
-                    "url": "/Stack/html5.svg"
+                    name: "HTML 5",
+                    url: "/Stack/html5.svg"
                 },
                 {
-                    "name": "CSS 3",
-                    "url": "/Stack/css3.svg"
+                    name: "CSS 3",
+                    url: "/Stack/css3.svg"
                 },
                 {
-                    "name": "Sass",
-                    "url": "/Stack/sass.svg"
+                    name: "Sass",
+                    url: "/Stack/sass.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Create a login system to allow users to save their favorite restaurants",
                 "Create a system to book reservations",
                 "Create a restaurant access for them to manage their own menu"
             ],
-            "tags": "#React #HTML5 #CSS3  #Sass",
-            "github": "https://github.com/Jerome-Baille/ohmyfood",
-            "demo": "https://jerome-baille.github.io/ohmyfood/"
+            tags: "#React #HTML5 #CSS3  #Sass",
+            github: "https://github.com/Jerome-Baille/ohmyfood",
+            demo: "https://jerome-baille.github.io/ohmyfood/"
         },
         "LaChouetteAgence": {
-            "id": "LaChouetteAgence",
-            "logo": "/Projects/LaChouetteAgence/laChouetteAgence-logo.png",
-            "pictures": [
+            id: "LaChouetteAgence",
+            logo: "/Projects/LaChouetteAgence/laChouetteAgence-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/LaChouetteAgence/mokup1.png",
+                    url: "/Projects/LaChouetteAgence/mokup1.png",
                     "alt": "La Chouette Agence - Mokup 1"
                 },
                 {
-                    "url": "/Projects/LaChouetteAgence/mokup2.png",
-                    "caption": "La Chouette Agence - Mokup 2"
+                    url: "/Projects/LaChouetteAgence/mokup2.png",
+                    caption: "La Chouette Agence - Mokup 2"
                 }
             ],
-            "name": "La Chouette Agence",
-            "subtitle": "Optimization of an existing website",
-            "descriptions" : [
+            name: "La Chouette Agence",
+            subtitle: "Optimization of an existing website",
+            descriptions : [
                 "La chouette agence is a large web design agency based in Lyon, France.",
                 "Due to SEO problems, the website was only on the second page of search engines.",
                 "An SEO optimization was therefore performed and an improvement of the website's code in order to correct the display errors and improve the website's accessibility."
             ],
-            "skills": [
+            skills: [
                 "Optimizing the size and speed of a website",
                 "Ensuring the accessibility of a website",
                 "Optimizing the referencing of a website",
                 "Researching best practices in web development",
                 "Writing maintainable HTML and CSS code"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "HTML 5",
-                    "url": "/Stack/html5.svg"
+                    name: "HTML 5",
+                    url: "/Stack/html5.svg"
                 },
                 {
-                    "name": "CSS 3",
-                    "url": "/Stack/css3.svg"
+                    name: "CSS 3",
+                    url: "/Stack/css3.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Modify the format of the different images to improve speed and size"
             ],
-            "tags": "#SEO  #WebAccessibility  #audit",
-            "github": "https://github.com/Jerome-Baille/JeromeBaille_4_09122021",
-            "demo": "https://jerome-baille.github.io/JeromeBaille_4_09122021/"
+            tags: "#SEO  #WebAccessibility  #audit",
+            github: "https://github.com/Jerome-Baille/JeromeBaille_4_09122021",
+            demo: "https://jerome-baille.github.io/JeromeBaille_4_09122021/"
         },
         "Kanap": {
-            "id": "Kanap",
-            "logo": "/Projects/Kanap/kanap-logo.png",
-            "pictures": [
+            id: "Kanap",
+            logo: "/Projects/Kanap/kanap-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/Kanap/mokup1.png",
+                    url: "/Projects/Kanap/mokup1.png",
                     "alt": "Kanap - Mokup 1"
                 },
                 {
-                    "url": "/Projects/Kanap/mokup2.png",
-                    "caption": "Kanap - Mokup 2"
+                    url: "/Projects/Kanap/mokup2.png",
+                    caption: "Kanap - Mokup 2"
                 }
             ],
-            "name": "Kanap",
-            "subtitle": "Building an e-commerce website in JavaScript",
-            "descriptions" : [
+            name: "Kanap",
+            subtitle: "Building an e-commerce website in JavaScript",
+            descriptions : [
                 "Kanap is a sofa brand that sells its products exclusively from its store.",
                 "Today, the company wanted to have an e-commerce platform in addition to its physical store to sell its products on the Internet."
             ],
-            "skills": [
+            skills: [
                 "Validating data from external sources",
                 "Managing JavaScript events",
                 "Creating a test plan for an application",
                 "Interacting with a web service using JavaScript"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "HTML 5",
-                    "url": "/Stack/html5.svg"
+                    name: "HTML 5",
+                    url: "/Stack/html5.svg"
                 },
                 {
-                    "name": "CSS 3",
-                    "url": "/Stack/css3.svg"
+                    name: "CSS 3",
+                    url: "/Stack/css3.svg"
                 },
                 {
-                    "name": "JavaScript",
-                    "url": "/Stack/javascript.svg"
+                    name: "JavaScript",
+                    url: "/Stack/javascript.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Create a login system to allow users to save their favorite products",
                 "Create a login system to allow users to modify and/or keep track of their order"
             ],
-            "tags": "#e-commerce  #JavaScript",
-            "github": "https://github.com/Jerome-Baille/JeromeBaille_5_03012022",
-            "demo": "https://jerome-baille.github.io/JeromeBaille_5_03012022"
+            tags: "#e-commerce  #JavaScript",
+            github: "https://github.com/Jerome-Baille/JeromeBaille_5_03012022",
+            demo: "https://jerome-baille.github.io/JeromeBaille_5_03012022"
         },
         "Piiquante": {
-            "id": "Piiquante",
-            "logo": "/Projects/Piiquante/piiquante-logo.png",
-            "pictures": [
+            id: "Piiquante",
+            logo: "/Projects/Piiquante/piiquante-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/Piiquante/mokup1.png",
+                    url: "/Projects/Piiquante/mokup1.png",
                     "alt": "Piiquante - Mokup 1"
                 },
                 {
-                    "url": "/Projects/Piiquante/mokup2.png",
-                    "caption": "Piiquante - Mokup 2"
+                    url: "/Projects/Piiquante/mokup2.png",
+                    caption: "Piiquante - Mokup 2"
                 }
             ],
-            "name": "Piiquante",
-            "subtitle": "Building an API for a food review application",
-            "descriptions" : [
+            name: "Piiquante",
+            subtitle: "Building an API for a food review application",
+            descriptions : [
                 "Piiquante is a hot sauce company that wants to develop a hot sauce review web application called 'Hot Takes'.",
                 "While Piiquante's product manager hopes to eventually turn the review application into an online store, she wants the first version to be a 'sauce gallery' that allows users to upload their favorite hot sauces and to like or dislike the sauces that others share."
             ],
-            "skills": [
+            skills: [
                 "Implement a logical data model in accordance with regulations",
                 "Implement CRUD operations in a secure manner",
                 "Store data in a secure manner"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "Node.js",
-                    "url": "/Stack/nodejs.svg"
+                    name: "Node.js",
+                    url: "/Stack/nodejs.svg"
                 },
                 {
-                    "name": "Express.js",
-                    "url": "/Stack/express.svg"
+                    name: "Express.js",
+                    url: "/Stack/express.svg"
                 },
                 {
-                    "name": "MongoDB",
-                    "url": "/Stack/mongodb.svg"
+                    name: "MongoDB",
+                    url: "/Stack/mongodb.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Create an online store to allow users to buy hot sauces",
                 "Modify the code to prevent users from leaving the app on refresh"
             ],
-            "tags": "#API  #Node.js  #Express.js  #MongoDB",
-            "github": "https://github.com/Jerome-Baille/JeromeBaille_6_24012022"
+            tags: "#API  #Node.js  #Express.js  #MongoDB",
+            github: "https://github.com/Jerome-Baille/JeromeBaille_6_24012022"
         },
         "Groupomania": {
-            "id": "Groupomania",
-            "logo": "/Projects/Groupomania/groupomania-logo.png",
-            "pictures": [
+            id: "Groupomania",
+            logo: "/Projects/Groupomania/groupomania-logo.png",
+            pictures: [
                 {
-                    "url": "/Projects/Groupomania/mokup1.png",
+                    url: "/Projects/Groupomania/mokup1.png",
                     "alt": "Groupomania - Mokup 1"
                 },
                 {
-                    "url": "/Projects/Groupomania/mokup2.png",
-                    "caption": "Groupomania - Mokup 2"
+                    url: "/Projects/Groupomania/mokup2.png",
+                    caption: "Groupomania - Mokup 2"
                 }
             ],
-            "name": "Groupomania",
-            "subtitle": "Creation of a company social network",
-            "descriptions" : [
+            name: "Groupomania",
+            subtitle: "Creation of a company social network",
+            descriptions : [
                 "Groupomania is a group specialized in mass distribution.",
                 "The project consists in building an internal social network for the employees of Groupomania.",
                 "The goal of this tool is to facilitate interactions between colleagues."
             ],
-            "skills": [
+            skills: [
                 "Authenticating a user and maintaining their session",
                 "Managing a data store using SQL",
                 "Implementing secure data storage using SQL",
                 "Customize the content sent to a web client"
             ],
-            "stack": [
+            stack: [
                 {
-                    "name": "Angular",
-                    "url": "/Stack/angular.svg"
+                    name: "Angular",
+                    url: "/Stack/angular.svg"
                 },
                 {
-                    "name": "Sass",
-                    "url": "/Stack/sass.svg"
+                    name: "Sass",
+                    url: "/Stack/sass.svg"
                 },
                 {
-                    "name": "Node.js",
-                    "url": "/Stack/nodejs.svg"
+                    name: "Node.js",
+                    url: "/Stack/nodejs.svg"
                 },
                 {
-                    "name": "Express.js",
-                    "url": "/Stack/express.svg"
+                    name: "Express.js",
+                    url: "/Stack/express.svg"
                 },
                 {
-                    "name": "MySQL",
-                    "url": "/Stack/mysql.svg"
+                    name: "MySQL",
+                    url: "/Stack/mysql.svg"
                 }
             ],
-            "improvements": [
+            improvements: [
                 "Change the comment system to a nested one to allow for replies",
                 "Implement a system to resize user's profile picture"
             ],
-            "tags": "#Angular  #Node.js  #Express.js  #MySQL  #SocialNetwork",
-            "github": "https://github.com/Jerome-Baille/JeromeBaille_7_23022022"
+            tags: "#Angular  #Node.js  #Express.js  #MySQL  #SocialNetwork",
+            github: "https://github.com/Jerome-Baille/JeromeBaille_7_23022022"
         }
 };
 
