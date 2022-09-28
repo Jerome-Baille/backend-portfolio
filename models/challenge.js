4352e9a015a933b09f89eaae1d91cbbc53910c91
Jerome-Baille <baille.jerome@gmail.com>
2022-09-28 09:20:03 +0200

    Modify the challenge section

--- models/challenge.js
+++ models/challenge.js
@@ -4,8 +4,9 @@ const challenges = [
         status: 'Active',
         name: '100 days CSS Challenge',
         description: [
-            "The challenge is to reproduce a given HTML and CSS snippets for 100 consecutive days.",
-            "Participants use CodePen to write and save their code and publish the results every day on the challenge website."
+            "The challenge is to reproduce a given HTML, CSS snippets for 100 consecutive days.",
+            "The code need to be written and saved on CodePen",
+            "Once it is done, the CodePen link has to be published every day on the challenge website."
         ],
         url: 'https://100dayscss.com/'
     },
@@ -14,8 +15,8 @@ const challenges = [
         status: 'Active',
         name: 'Google Kick Start 2022',
         description: [
-            "Kick Start offers coders around the world the chance to develop and hone their programming skills through online-hosted competition rounds.",
-            "The three-hour rounds feature a variety of algorithmic challenges, all developed by Google engineers so that you get a taste of the technical skills needed for a career at Google (the top competitors from our Kick Start rounds may be invited to interview at Google!)."
+            "Kick Start is an online coding competition divided in rounds.",
+            "The three-hour rounds feature a variety of algorithmic challenges, all developed by Google engineers (the top competitors from the Kick Start rounds may be invited to interview at Google!)."
         ],
         url: 'https://codingcompetitions.withgoogle.com/kickstart/about'
     }
